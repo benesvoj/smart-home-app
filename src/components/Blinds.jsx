1878bcf20bb8639261f23b7e9c1e7a56fe1f1ba9
Vojtech Benes <vojtech.benes@carvago.com>
2024-01-22 13:06:43 +0100

    blinds functinalities

--- src/components/Blinds.jsx
+++ src/components/Blinds.jsx
@@ -1,15 +1,30 @@
+import {useState} from "react";
+
 export const Blinds = () => {
+
+    const [blinds, setBlinds] = useState('blinds-open')
+
+    const openBlinds = () => {
+        setBlinds('blinds-open')
+    }
+
+    const closeBlinds = () => {
+        setBlinds('blinds-closed')
+    }
+
+    const activeOpenButton = blinds === 'blinds-open' ? 'button button--active' : 'button'
+    const activeCloseButton = blinds === 'blinds-closed' ? 'button button--active' : 'button'
     return (
         <div className="blinds">
             <div className="blinds__icon">
-                <img src="/assets/images/blinds-open.svg" alt={"blinds"}/>
+                <img src={`/assets/images/${blinds}.svg`} alt={"blinds"}/>
             </div>
             <div className="blinds__name">
                 Žaluzie
             </div>
             <div className="blinds__controls">
-                <button className="button button--active">Otevřeno</button>
-                <button className="button">Zavřeno</button>
+                <button className={activeOpenButton} onClick={openBlinds}>Otevřeno</button>
+                <button className={activeCloseButton} onClick={closeBlinds}>Zavřeno</button>
             </div>
         </div>
     )
