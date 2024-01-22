50f6d3c5013f01bce39bc34355bdb32ebdc25e32
Vojtech Benes <vojtech.benes@carvago.com>
2024-01-22 12:48:49 +0100

    add functionality to humidity

--- src/components/Climate.jsx
+++ src/components/Climate.jsx
@@ -3,14 +3,18 @@ import {climateData} from "../utils/data.js";
 
 export const Climate = () => {
     const initialTemperature = climateData.filter(item => item.type === "temperature")[0].value
+    const initialHumidity = climateData.filter(item => item.type === "humidity")[0].value
 
     const [temperature, setTemperature] = useState(initialTemperature)
+    const [humidity, setHumidity] = useState(initialHumidity)
 
     const increaseTemp = () => {
         setTemperature(temperature + 1)
+        setHumidity(humidity - 2)
     }
     const decreaseTemp = () => {
         setTemperature(temperature - 1)
+        setHumidity(humidity + 2)
     }
 
     return (
@@ -20,7 +24,7 @@ export const Climate = () => {
             </div>
             <div className="climate__content">
                 <div className="climate__temperature">{temperature}&deg;C</div>
-                <div className="climate__humidity">Vlhkost vzduchu 51&nbsp;%</div>
+                <div className="climate__humidity">Vlhkost vzduchu {humidity}&nbsp;%</div>
             </div>
             <div className="climate__controls">
                 <button onClick={increaseTemp} className="button">+</button>
